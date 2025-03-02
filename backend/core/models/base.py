b8d2bc7019e7b3162298b5184b891915128cc1c0
ya.dvoynih <ya.dvoynih@tensor.ru>
2025-03-02 23:34:14 +0300

    project template

--- backend/core/models/base.py
+++ backend/core/models/base.py
@@ -1,5 +1,5 @@
 from sqlalchemy import MetaData
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
+from sqlalchemy.orm import DeclarativeBase, declared_attr
 
 from utils import camel_case_to_snake_case
 from core.config import settings
@@ -15,5 +15,3 @@ class Base(DeclarativeBase):
     @declared_attr.directive
     def __tablename__(cls) -> str:
         return f'{camel_case_to_snake_case(cls.__name__)}s'
-
-    id: Mapped[int] = mapped_column(primary_key=True)
--- backend/core/models/player.py
+++ backend/core/models/player.py
@@ -2,8 +2,8 @@ from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 
 from .base import Base
-#from .mixins.int_id_pk import IntIdPkMixin
+from .mixins.int_id_pk import IntIdPkMixin
 
 
-class Player(Base):
+class Player(IntIdPkMixin, Base):
     first_name: Mapped[str] = mapped_column(unique=True)
