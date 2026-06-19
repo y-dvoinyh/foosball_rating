c483acaf5b7f2e83a0f7ad55a8fe4610a1322833
ya.dvoynih <ya.dvoynih@tensor.ru>
2026-06-19 10:16:13 +0300

    fix: use default celery queue

--- backend/app/core/celery_app.py
+++ backend/app/core/celery_app.py
@@ -15,9 +15,6 @@ celery_app.conf.update(
     timezone="UTC",
     enable_utc=True,
     task_track_started=True,
-    task_routes={
-        "app.tasks.debug.*": {"queue": "default"},
-    },
     beat_schedule={
         "debug-ping-every-hour": {
             "task": "app.tasks.debug.ping",
